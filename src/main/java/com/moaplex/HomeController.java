626c52f590637e25c7139e0295d3e75bc55fa6d5
hanna05 <hanna0836@naver.com>
2016-08-17 15:15:46 +0900

    dddd

---
+++ src/main/java/com/moaplex/HomeController.java
@@ -0,0 +1,17 @@
+package com.moaplex;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hanna on 2016-08-17.
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping
+    public String test() {
+        return "Helloworld";
+    }
+
+}
